d3f2c19a57da751ba9921edb870c1429331a0552
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-19 09:01:55 +0200

    tm-07: + passwordInput component

--- frontend/src/pages/auth/libs/components/components.ts
+++ frontend/src/pages/auth/libs/components/components.ts
@@ -1,3 +1,4 @@
 export { AuthLayout } from "./auth-layout/auth-layout.js";
+export { PasswordInput } from "./password-input/password-input.js";
 export { SignInForm } from "./sign-in-form/sign-in-form.js";
 export { SignUpForm } from "./sign-up-form/sign-up-form.js";

6f7dc14da2dc38f82acb4b365d0eedea7122cab4
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-19 09:00:48 +0200

    tm-07: * authLayout component

--- frontend/src/pages/auth/libs/components/auth-layout/auth-layout.tsx
+++ frontend/src/pages/auth/libs/components/auth-layout/auth-layout.tsx
@@ -1,4 +1,5 @@
-import authBackground from "~/assets/images/auth-background.png";
+import { Icon } from "@/components/components.js";
+import authBackground from "~/assets/images/auth-background.png?url";
 
 import styles from "./styles.module.scss";
 
@@ -14,7 +15,16 @@ const AuthLayout: React.FC<Properties> = ({ children }) => {
         width="960"
         height="1080"
       />
-      <div className={styles.formWrapper}>{children}</div>
+      <div className={styles.formWrapper}>
+        <header className={styles.header}>
+          <Icon iconName="teamderLogo" className={styles.logo} />
+          <div className={styles.descriptionWrapper}>
+            <h1 className={styles.title}>Teamder</h1>
+            <div className={styles.description}>Find your dreamteam</div>
+          </div>
+        </header>
+        {children}
+      </div>
     </div>
   );
 };

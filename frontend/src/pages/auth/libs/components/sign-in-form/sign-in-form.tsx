c76047cffcbe824ba4b064869c2b76328c30c42b
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-19 09:02:46 +0200

    tm-07: + signInForm component

--- frontend/src/pages/auth/libs/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/pages/auth/libs/components/sign-in-form/sign-in-form.tsx
@@ -1,12 +1,67 @@
+import { Button, Checkbox, Input, Link } from "@/components/components.js";
+import { AppRoute } from "@/enums/enums.js";
+import { useAppForm } from "@/hooks/hooks.js";
+import {
+  type UserSignInRequestDto,
+  userSignInValidationSchema,
+} from "#/users/users.js";
+
+import { PasswordInput } from "../components.js";
+import { DEFAULT_SIGN_IN_PAYLOAD } from "./libs/constants/constants.js";
+import styles from "./styles.module.scss";
+
 type Properties = {
   onSubmit: () => void;
 };
 
-const SignInForm: React.FC<Properties> = () => (
-  <>
-    <h1>Sign In</h1>
-    <form></form>
-  </>
-);
+const SignInForm: React.FC<Properties> = () => {
+  const { control, errors } = useAppForm<UserSignInRequestDto>({
+    defaultValues: DEFAULT_SIGN_IN_PAYLOAD,
+    validationSchema: userSignInValidationSchema,
+  });
+
+  return (
+    <form>
+      <div className={styles.wrapper}>
+        <h2 className={styles.header}>Welcome, login to your account!</h2>
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          label="Email"
+          control={control}
+          errors={errors}
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          placeholder="Password"
+          control={control}
+          errors={errors}
+        />
+        <div className={styles.details}>
+          <Checkbox
+            name="rememberMe"
+            label="Remember me"
+            showLabel
+            control={control}
+            errors={errors}
+          />
+          <Link to={AppRoute.FORGOT_PASSWORD} className={styles.forgotPassword}>
+            Forgot Password?
+          </Link>
+        </div>
+      </div>
+      <Button
+        label="Sign in"
+        className={styles.signInButton}
+        onClick={(): void => {}}
+      />
+      <div className={styles.createAccountDescription}>
+        Don&apos;t have an account yet?&nbsp;<span>Sign up!</span>
+      </div>
+    </form>
+  );
+};
 
 export { SignInForm };

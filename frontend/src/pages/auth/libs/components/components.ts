e471a93b01a6c5c0eb00b3b8c5b438c7bd840907
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-26 15:42:16 +0200

    tm-21: + signUpInterestsForm component

--- frontend/src/pages/auth/libs/components/components.ts
+++ frontend/src/pages/auth/libs/components/components.ts
@@ -1,4 +1,7 @@
 export { AuthLayout } from "./auth-layout/auth-layout.js";
 export { PasswordInput } from "./password-input/password-input.js";
 export { SignInForm } from "./sign-in-form/sign-in-form.js";
-export { SignUpForm } from "./sign-up-form/sign-up-form.js";
+export { SignUpAdditionalInfoForm } from "./sign-up-additional-info-form/sign-up-additional-info-form.js";
+export { SignUpBasicForm } from "./sign-up-basic-form/sign-up-basic-form.js";
+export { SignUpInterestsForm } from "./sign-up-interests-form/sign-up-interests-form.js";
+export { SignUpProgress } from "./sign-up-progress/sign-up-progress.js";
---
+++ frontend/src/pages/auth/libs/components/sign-up-interests-form/sign-up-interests-form.tsx
@@ -0,0 +1,66 @@
+import { Button, Checkbox, Link } from "@/components/components.js";
+import { AppRoute } from "@/enums/enums.js";
+import { makeFirstLetterUppercase } from "@/helpers/helpers.js";
+import { useAppForm } from "@/hooks/hooks.js";
+import {
+  userSignInValidationSchema,
+  type UserSignUpInterests,
+} from "#/users/users.js";
+
+import { DEFAULT_SIGN_UP_INTERESTS_PAYLOAD } from "../../constants/constants.js";
+import { SignUpProgressStep } from "../../enums/enums.js";
+import { SignUpProgress } from "../components.js";
+import styles from "./styles.module.scss";
+
+type Properties = {
+  onSubmit: () => void;
+};
+
+const SignUpInterestsForm: React.FC<Properties> = () => {
+  const { control, errors } = useAppForm<UserSignUpInterests>({
+    defaultValues: DEFAULT_SIGN_UP_INTERESTS_PAYLOAD,
+    validationSchema: userSignInValidationSchema,
+  });
+
+  return (
+    <form>
+      <div className={styles.wrapper}>
+        <SignUpProgress step={SignUpProgressStep.THIRD} />
+        <h2 className={styles.header}>Interests</h2>
+        <ul className={styles.interestsWrapper}>
+          {(
+            Object.keys(
+              DEFAULT_SIGN_UP_INTERESTS_PAYLOAD,
+            ) as (keyof UserSignUpInterests)[]
+          ).map((interest) => {
+            return (
+              <li key={interest}>
+                <Checkbox
+                  name={interest}
+                  control={control}
+                  errors={errors}
+                  label={makeFirstLetterUppercase(interest)}
+                  checkboxStyle="square"
+                  showLabel
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Button
+        label="Sign Up"
+        className={styles.signUpButton}
+        onClick={(): void => {}}
+      />
+      <div className={styles.signInDescription}>
+        Already have an account?&nbsp;
+        <Link to={AppRoute.SIGN_IN} className={styles.signInLink}>
+          Sign In!
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export { SignUpInterestsForm };

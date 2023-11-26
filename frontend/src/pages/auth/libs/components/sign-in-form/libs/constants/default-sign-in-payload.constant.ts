982441e160ca05edf44c95c47a70b931518c47a8
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-26 15:29:52 +0200

    tm-21: * auth pages folders structure

--- frontend/src/pages/auth/libs/components/sign-in-form/libs/constants/default-sign-in-payload.constant.ts
+++
@@ -1,9 +0,0 @@
-import { type UserSignInRequestDto } from "#/users/users.js";
-
-const DEFAULT_SIGN_IN_PAYLOAD: UserSignInRequestDto = {
-  email: "",
-  password: "",
-  rememberMe: false,
-};
-
-export { DEFAULT_SIGN_IN_PAYLOAD };

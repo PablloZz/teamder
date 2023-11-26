2e076837639b2c31857d02ca266478726ab52d22
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-26 15:45:57 +0200

    tm-21: * app routes

--- frontend/src/libs/enums/app-route.enum.ts
+++ frontend/src/libs/enums/app-route.enum.ts
@@ -2,7 +2,9 @@ const AppRoute = {
   ROOT: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
-  FORGOT_PASSWORD: "/forgot-password",
+  SIGN_UP_ADDITIONAL_INFO: "/sign-up/additional-info",
+  SIGN_UP_INTERESTS: "/sign-up/interests",
+  FORGOT_PASSWORD: "/sign-up/forgot-password",
 } as const;
 
 export { AppRoute };

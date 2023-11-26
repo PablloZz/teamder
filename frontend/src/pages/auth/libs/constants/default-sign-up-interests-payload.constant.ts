62365b70c6ee87c639debf94de44d54292745cbb
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-26 15:28:16 +0200

    tm-21: + default sign-up payload constants

---
+++ frontend/src/pages/auth/libs/constants/default-sign-up-interests-payload.constant.ts
@@ -0,0 +1,14 @@
+import { type UserSignUpInterests } from "#/users/users.js";
+
+const DEFAULT_SIGN_UP_INTERESTS_PAYLOAD: UserSignUpInterests = {
+  sport: false,
+  programming: false,
+  swimming: false,
+  dance: false,
+  bowling: false,
+  traveling: false,
+  singing: false,
+  music: false,
+};
+
+export { DEFAULT_SIGN_UP_INTERESTS_PAYLOAD };

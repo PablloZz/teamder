d4b54be75fd66bcb67c5aafd626e373f88ee1575
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-26 15:33:58 +0200

    tm-21: * auth pages constants folder structure

---
+++ frontend/src/pages/auth/libs/constants/constants.ts
@@ -0,0 +1,4 @@
+export { DEFAULT_SIGN_IN_PAYLOAD } from "./default-sign-in-payload.constant.js";
+export { DEFAULT_SIGN_UP_ADDITIONAL_INFO_PAYLOAD } from "./default-sign-up-additional-info-payload.constant.js";
+export { DEFAULT_SIGN_UP_BASIC_PAYLOAD } from "./default-sign-up-basic-payload.constant.js";
+export { DEFAULT_SIGN_UP_INTERESTS_PAYLOAD } from "./default-sign-up-interests-payload.constant.js";

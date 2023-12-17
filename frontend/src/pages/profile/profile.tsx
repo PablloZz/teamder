e7932d5dc2f370cd56731f8c233974bc5b5697ee
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-12-17 16:18:48 +0200

    tm-25: + profile component and routes

---
+++ frontend/src/pages/profile/profile.tsx
@@ -0,0 +1,54 @@
+import { Layout, Link, RouterOutlet } from "@/components/components.js";
+import { AppRoute } from "@/enums/enums.js";
+
+import { UserCard } from "./libs/components/components.js";
+import styles from "./styles.module.scss";
+
+const Profile: React.FC = () => {
+  return (
+    <Layout title="My profile">
+      <div className={styles.wrapper}>
+        <UserCard />
+        <nav className={styles.navigation}>
+          <ul className={styles.navigationList}>
+            <li className={styles.listItem}>
+              <Link
+                to={AppRoute.PROFILE_$ID_INTERESTS}
+                className={styles.listItemLink}
+                activeClassName={styles.activeListItemLink}
+              >
+                Interests
+              </Link>
+            </li>
+            <li className={styles.listItem}>
+              <Link
+                to={AppRoute.PROFILE_$ID_SOCIAL_MEDIA}
+                className={styles.listItemLink}
+                activeClassName={styles.activeListItemLink}
+              >
+                Social Media
+              </Link>
+            </li>
+            <li className={styles.listItem}>
+              <Link
+                to={AppRoute.PROFILE_$ID_EDIT_PROFILE}
+                className={styles.listItemLink}
+                activeClassName={styles.activeListItemLink}
+              >
+                Edit Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <RouterOutlet />
+      </div>
+    </Layout>
+  );
+};
+
+export { Profile };
+export {
+  EditProfile,
+  Interests,
+  SocialMedia,
+} from "./libs/components/components.js";

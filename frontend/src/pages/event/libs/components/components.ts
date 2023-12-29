63ba8cd2e45c1df518c28cfe8408ca9c8ba4a32e
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-12-29 16:09:08 +0200

    tm-31: + re-export

---
+++ frontend/src/pages/event/libs/components/components.ts
@@ -0,0 +1,3 @@
+export { EventDescription } from "./event-description/event-description.js";
+export { EventForum } from "./event-forum/event-forum.js";
+export { EventMembers } from "./event-members/event-members.js";

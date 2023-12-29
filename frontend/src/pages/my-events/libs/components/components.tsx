07aa4de56d2e5c30dba8988d4bb429c5c9f234d5
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-12-29 15:53:19 +0200

    tm-31: + my completed/current events components

---
+++ frontend/src/pages/my-events/libs/components/components.tsx
@@ -0,0 +1,3 @@
+export { EventOverviewCard } from "./event-overview-card/event-overview-card.js";
+export { MyCompletedEvents } from "./my-completed-events/my-completed-events.js";
+export { MyCurrentEvents } from "./my-current-events/my-current-events.js";
